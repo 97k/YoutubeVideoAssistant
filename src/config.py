1d430df21b183c5e0a7de871ff1f73514bb595e6
Aditya Kaushik <97k.work@gmail.com>
2024-04-03 08:49:11 +0530

    Add .pre-commit & clean code files

--- src/config.py
+++ src/config.py
@@ -1,7 +1,11 @@
 import langchain.pydantic_v1 as pyd
 
+
 class Settings(pyd.BaseSettings):
     HF_TOKEN: str
-    HF_INF_API: pyd.AnyUrl = "https://api-inference.huggingface.co/models" # to support any TGI hosted API
+    HF_INF_API: pyd.AnyUrl = (
+        "https://api-inference.huggingface.co/models"  # to support any TGI hosted API
+    )
+
 
 settings = Settings()
--- src/logger.py
+++ src/logger.py
@@ -4,9 +4,9 @@ logging.basicConfig(
     filename="./yt_qa_sum.log",
     format="%(asctime)s %(name)s %(levelname)s %(module)s %(funcName)s %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S",
-    level=logging.INFO
+    level=logging.INFO,
 )
 
 
-def get_logger(name: str|None = None):
+def get_logger(name: str | None = None):
     return logging.getLogger(name=name)
